16d4928b12bd05969f6b1987809b7bc0f21fac48
Yaroslav.Turchenko <yaroslav.turchenko@tellurionmobile.com>
2021-07-12 16:20:16 +0300

    fix max

--- task1.py
+++ task1.py
@@ -4,9 +4,12 @@ nums = re.findall(r'\d+', list)
 letters = re.sub(r'\d', '', list)
 letters_f = ' '.join(part[0].upper() + part[1:-1] + part[-1:].upper() for part in letters.split()) #змінюємо нульовий та -1 символи в кожній частині 
 int_nums = [int(item) for item in nums]
-to_the_power_of_two = [num**int_nums.index(num) for num in int_nums if num != max(int_nums)]
-print(int_nums, letters, letters_f, to_the_power_of_two, int_nums)
+if not int_nums:
+    print("There're no numbers in the string")
+else:
+    print(int_nums)
+    print("Max numer is:",max(int_nums))
+    to_the_power_of_two = [num**int_nums.index(num) for num in int_nums if num != max(int_nums)]
+    print(to_the_power_of_two)
 print(letters)
 print(letters_f)
-print(to_the_power_of_two)
-print(max(int_nums))
